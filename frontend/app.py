afee4e7f406e7e09bf867deb916989b4d5c6f515
Jovin Ang <36396502+Jovin-Ang@users.noreply.github.com>
2025-08-28 15:21:03 +0000

    Add poetry

---
+++ frontend/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# from streamlit_supabase_auth import login_form, logout_button
+
+
+def main():
+    st.title("Component Gallery")
+    st.header("Login with Supabase Auth")
+
+
+if __name__ == "__main__":
+    main()
